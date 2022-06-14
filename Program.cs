ea04571335a59c57f73fa03fabfa0c309badd910
Marcos Alberto <m.albertoconceicao@gmail.com>
2022-06-14 11:12:13 -0300

    EstruturaCronometro

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Cronometro
     {
         static void Main(string[] args)
         {
-            
+
+        }
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {
+                currentTime++;
+            }
         }
     }
 }
