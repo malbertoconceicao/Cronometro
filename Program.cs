a96f3bf67045c555cafef9c2ccd05f2079be7eb0
Marcos Alberto <m.albertoconceicao@gmail.com>
2022-06-14 11:28:57 -0300

    OpcoesMenu

--- Program.cs
+++ Program.cs
@@ -11,10 +11,14 @@ namespace Cronometro
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segundos");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+
+
         }
         static void Start(int time)
         {
