8d2f094a7886feb0d70fa9ab24fa861002bf9ac1
Marcos Alberto <m.albertoconceicao@gmail.com>
2022-06-14 11:23:59 -0300

    CriandoMenu

--- Program.cs
+++ Program.cs
@@ -6,11 +6,18 @@ namespace Cronometro
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundos");
+            Console.WriteLine("M = Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+        }
+        static void Start(int time)
+        {
             int currentTime = 0;
 
             while(currentTime != time)
@@ -20,6 +27,9 @@ namespace Cronometro
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+            Console.Clear();
+            Console.WriteLine("Cronometro finalizado");
+            Thread.Sleep(2500);
         }
     }
 }
